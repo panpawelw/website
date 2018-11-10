776aecdd96bd38a1245aaae6ab59811f1438fe3b
Paweł Wojtuń <31450760+pjm77@users.noreply.github.com>
2018-11-10 23:59:45 +0100

    add curtain with info (not working yet)

--- js/main.js
+++ js/main.js
@@ -6,4 +6,13 @@ $(document).ready(function() {
 			$('.navbar').removeClass('transparent');
 		}
 	});
+	/* Open when someone clicks on the span element */
+	function openNav() {
+		document.getElementById("myNav").style.width = "100%";
+	}
+
+	/* Close when someone clicks on the "x" symbol inside the overlay */
+	function closeNav() {
+		document.getElementById("myNav").style.width = "0%";
+	}
 });
