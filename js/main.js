362e4e64b171feeb3bdb967bdf3e1713f72d4553
Paweł Wojtuń <31450760+pjm77@users.noreply.github.com>
2018-11-11 01:33:40 +0100

    curtain working fine now

--- js/main.js
+++ js/main.js
@@ -8,11 +8,13 @@ $(document).ready(function() {
 	});
 	/* Open when someone clicks on the span element */
 	function openNav() {
-		document.getElementById("myNav").style.width = "100%";
+		document.getElementById("myNav").style.height = "100%";
 	}
 
 	/* Close when someone clicks on the "x" symbol inside the overlay */
 	function closeNav() {
-		document.getElementById("myNav").style.width = "0%";
+		document.getElementById("myNav").style.height = "0%";
 	}
+	openNav();
+	closeNav();
 });
